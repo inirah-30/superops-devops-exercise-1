c8a6a7b3d395bf5980ef01a781e8a9b4cad04de1
inirah-30 <jothiharinii@gmail.com>
2026-08-12 10:54:27 +0000

    Initial Terraform project structure

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {
+  description = "AWS region where the infrastructure will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for public subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for public subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "project_name" {
+  description = "Name used for resource naming and tagging"
+  type        = string
+  default     = "superops-loadbalancer"
+}
